cad41d8a012ae4f7ca01e4973c16b948322e9a14
Meenakshi Pal <pal.meenakshi@outlook.com>
2024-01-31 12:46:46 +1100

    array in java script

---
+++ src/test/java/com/java_script/basic3.js
@@ -0,0 +1,8 @@
+var marks = Array(6)
+
+var marks = [94,87,82,79,95,98]
+
+console.log(marks[3])       //this give you the output of third subject marks
+marks[1] = 91               // this is used to change the value
+console.log(marks)          // now the o/p should be updated 
+console.log(marks.length)   // will give the total no of items present in the array
